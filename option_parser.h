5c3409275c637281fbb81393bb853b770d99c0fb
Andre Renaud <andre@ignavus.net>
2019-09-29 14:26:34 +1300

    Added some header comments

--- option_parser.h
+++ option_parser.h
@@ -36,10 +36,49 @@ struct option_entry {
 	bool present; // Set after opt_parse to indicate if the option was supplied
 };
 
-void opt_parse_usage(int (*print_func)(const char *string, ...), const char *prog_name, const struct option_entry *options);
+/**
+ * Parse a list of option/parameters for a command
+ * @param nargs The number of items in 'args'
+ * @param args a list of strings to parse
+ * @param options The options to check for
+ * @return < 0 on failure, >= 0 on success
+ */
 int opt_parse(int nargs, char **args, struct option_entry *options);
+
+/**
+ * Display the help/usage for a given list of options
+ * @param print_func Function to be used to display the output, printf style
+ * @param prog_name The name of the program to display usage (typically argv[0])
+ * @param options The list of options to display the information for
+ */
+void opt_parse_usage(int (*print_func)(const char *string, ...), const char *prog_name, const struct option_entry *options);
+
+/**
+ * Splits a single string into the white space separated items
+ * @param line original single string of input (note: This will have nul bytes
+ *.       inserted to break up the elements
+ * @param output Array to store the pointers to the individual words in
+ * @param max_items The maximum number of items to store in 'output'
+ * @return < 0 on failure, numnber of items stored in 'output' on success
+ */
 int opt_parse_split_string(char *line, char **output, int max_items);
+
+/**
+ * Determines if an option was seen after a call to opt_parse
+ * Note: If the option was supplied using the 'long_opt' instead, this will still return true
+ * @param short_opt The short_opt of the option to check for
+ * @param options The list of options to check in
+ * @return false if short_opt was not seen by opt_parse, true if it was
+ */
 bool opt_parse_present(char short_opt, const struct option_entry *options);
+
+/**
+ * Determines if a long option was seen after a call to opt_parse
+ * Note: If the option was supplied using the 'short_opt' instead, this will still return true
+ * @param long_opt The long_opt of the option to check for
+ * @param options The list of options to check in
+ * @return false if long_opt was not seen by opt_parse, true if it was
+ */
 bool opt_parse_present_long(const char *long_opt, const struct option_entry *options);
 
 #ifdef __cplusplus
